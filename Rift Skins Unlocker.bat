486c2e3866b75a3666ed145d6312e2d1b0a908cf
AyeItsAxi <76423997+AyeItsAxi@users.noreply.github.com>
2021-09-15 00:27:03 -0700

    Add files via upload

---
+++ Rift Skins Unlocker.bat
@@ -0,0 +1,68 @@
+@echo off
+ 
+title Rift Skins Unlocker
+color 0a
+goto a
+
+:a
+ 
+ping localhost -n 2 >nul
+echo Loading
+ 
+ping localhost -n 4 >nul
+echo Loading .
+ 
+ping localhost -n 4 >nul
+echo Loading ..
+ 
+ping localhost -n 4 >nul
+echo Loading ...
+ 
+ping local host -n 6 >nul
+echo Loaded! Locating Shard.dll .
+ 
+ping local host -n 2 >nul
+echo Locating Shard.dll ..
+ 
+ping local host -n 4 >nul
+echo Shard.dll found! 
+echo Injecting code
+ 
+ping local host -n 6 >nul
+echo Injecting .
+ 
+ping local host -n 2 >nul
+echo Injecting ..
+ 
+ping local host -n 9 >nul
+echo Injecting ...
+ 
+ping local host -n 3 >nul
+echo Injecting ....
+ 
+ping local host -n 9 >nul
+echo Finished injecting!
+ 
+ping local host -n 2 >nul
+echo Patching .
+ 
+ping local host -n 2 >nul
+echo Patching ..
+ 
+ping local host -n 5 >nul
+echo Patching ...
+ 
+ping local host -n 3 >nul
+echo Patching ....
+ping local host -n 7 >nul
+echo Done!
+ping local host -n 5 >nul
+goto c
+
+:c
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+ping local host -n 20 >nul
+echo Are you that stupid? lmfao
+ping local host -n 2 >nul
+echo Made by 1 24 9-ette#9999
+pause
